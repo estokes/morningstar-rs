a28c9e6833b96f70340e018d9594497f19b33ec2
Eric Stokes <letaris@gmail.com>
2018-10-17 22:45:22 +0000

    fix stats according to specification

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,6 @@
 extern crate libmodbus_rs;
 #[macro_use]
 extern crate bitflags;
+extern crate half;
 
 pub mod prostar_mppt;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
