756fb2fc053418e2429daf6d176b642d81dde790
Eric Stokes <letaris@gmail.com>
2018-12-16 14:59:17 -0500

    add prostar example

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+/// There is a sub module for each hardware family, as well as a common error type.
+
 extern crate libmodbus_rs;
 #[macro_use]
 extern crate bitflags;
@@ -11,4 +13,6 @@ extern crate chrono;
 extern crate error_chain;
 
 pub mod error;
+/// For the Prostar MPPT family (should support all models), tested
+/// with a Prostar MPPT 40M.
 pub mod prostar_mppt;
