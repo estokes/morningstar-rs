b6f9438d70f605305eb22d538790da1bb1844693
Eric Stokes <letaris@gmail.com>
2018-12-16 15:15:24 -0500

    fix docs

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,26 @@ extern crate chrono;
 extern crate error_chain;
 
 pub mod error;
-/// For the Prostar MPPT family (should support all models), tested
-/// with a Prostar MPPT 40M.
+/**
+Interface with the Prostar MPPT (all models) as documented at
+http://support.morningstarcorp.com/wp-content/uploads/2015/12/PSMPPT_public-MODBUS-doc_v04.pdf
+
+# Examples
+```
+use morningstar::{error as mse, prostar_mppt as ps};
+use std::{thread::sleep, time::{Instant, Duration}};
+
+let con = ps::Connection::new("/dev/ttyUSB0", 1).expect("connection failed");
+println!("{}", con.stats().expect("failed to get stats"));
+// It doesn't like getting commands too quickly
+sleep(Duration::from_millis(500));
+
+// Stop charging the battery
+con.write_coil(ps::Coil::ChargeDisconnect, true).expect("failed to stop charging");
+sleep(Duration::from_millis(500));
+
+// Start Charging again
+con.write_coil(ps::Coil::ChargeDisconnect, false).expect("failed to start charging");
+```
+*/
 pub mod prostar_mppt;
